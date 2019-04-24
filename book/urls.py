981f04e0dded7a3c2171f738571ddd3bfc50f9fe
xenups <xenups@outlook.com>
2019-04-24 22:40:21 +0430

    permissions

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('books/', views.BookList.as_view()),
     path('books/<int:pk>/', views.BookDetail.as_view()),
+
 ]
--- book/views.py
+++ book/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics
 from book import serializers
 from book import models
-
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny, IsAdminUser
 
 # Create your views here.
+from book.permissions import IsLoggedInUserOrAdmin, IsAuthenticatedNotPost
+
+
 class BookList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedNotPost,)
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
