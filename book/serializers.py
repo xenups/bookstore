c504ebb4c67c9a5b72647cae1c8e0c985323d05c
xenups <xenups@outlook.com>
2019-04-29 23:28:06 +0430

    The post structure defined Post Serializer completely fixed

--- book/serializers.py
+++ book/serializers.py
@@ -23,11 +23,18 @@ class BookSerializer(serializers.ModelSerializer):
         fields = ('title', 'authors', 'publisher', 'publication_date')
 
     def create(self, validated_data):
+        # first we pop or get the the type of the data we want
+        publish_data = validated_data.pop('publisher')
         authors_data = validated_data.pop('authors')
-
+        # then we create an object  of book to put created data into it
         book = models.Book.objects.create(**validated_data)
-
+        # then we create an object by pop ed type and finally we put it into book then we save it
+        publish, created = models.Publisher.objects.get_or_create(name=publish_data['name'])
+        book.publisher = publish
+        book.save()
         for author in authors_data:
             author, created = models.Author.objects.get_or_create(first_name=author['first_name'])
             book.authors.add(author)
+
+        book.save()
         return book
