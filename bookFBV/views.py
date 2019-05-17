524d4a6e46935539475ca6e6eb49c486ed0e35a1
xenups <xenups@outlook.com>
2019-05-18 01:40:26 +0430

    ITs Class Based View Tutorial

--- bookFBV/views.py
+++ bookFBV/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # Function Base View Tutorial
 # Create your views here.
+# This Class Base View
 from rest_framework import generics, status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
